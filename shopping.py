05e42e0553d27a8828ecbb2fdbe53927b929d79c
janmarek20 <130707913+janmarek20@users.noreply.github.com>
2024-04-03 00:23:39 +0200

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -2,6 +2,8 @@ shopping = {'piekarnia': ['chleb', 'pączek', 'bułki'],
             'warzywniak': ['marchew', 'seler', 'rukola']
 }
 
+print('Hey')
+
 for shop, products in shopping.items():
     products_names = [product.capitalize() for product in products]
     all_products += products
