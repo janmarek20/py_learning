a21dfacc9f0df98090abc9259792e120517e6ebd
Jan Marek <janekmvi@gmail.com>
2024-04-02 23:11:17 +0200

    Add two messages

--- shopping.py
+++ shopping.py
@@ -5,3 +5,5 @@ shopping = {'piekarnia': ['chleb', 'pączek', 'bułki'],
 for shop, products in shopping.items():
     products_names = [product.capitalize() for product in products]
     all_products += products
+    print(f'Idę do {shop.capitalize()} i kupuję tam {products_names}')
+print(f'W sumie kupuję tam {len(all_products)} produktów.')
