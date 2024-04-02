fe98c513c2c4de45a7061455ddc88521591b9219
Jan Marek <janekmvi@gmail.com>
2024-04-02 23:09:56 +0200

    Change shops and products names

--- shopping.py
+++ shopping.py
@@ -1,3 +1,7 @@
 shopping = {'piekarnia': ['chleb', 'pączek', 'bułki'],
             'warzywniak': ['marchew', 'seler', 'rukola']
 }
+
+for shop, products in shopping.items():
+    products_names = [product.capitalize() for product in products]
+    all_products += products
